b78b201773792cd6ab4d9070a9dcdd06cf0d3be5
momogit <ferguson.tyra@gmail.com>
2021-05-08 17:09:21 -0400

    queries added to js and seed file

--- employee.js
+++ employee.js
@@ -1,50 +1,154 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: '',
-  database: 'employee_DB',
+  user: "root",
+  password: "",
+  database: "employee_DB",
 });
 
-
 connection.connect((err) => {
-    if (err) throw err;
-    employeeSearch();
-  });
-  
+  if (err) throw err;
+  generalSearch();
+});
 
-const employeeSearch = () => {
-    inquirer
-      .prompt({
-        name: 'action',
-        type: 'rawlist',
-        message: 'What would you like to do?',
-        choices: [
-          'Find employee by name',
-          'Search roles',
-          'Search departments',
-        ],
-      })
-      .then((answer) => {
-        switch (answer.action) {
-          case 'Find employee by name':
-            employeeSearch();
-            break;
-  
-          case 'Search roles':
-            roleSearch();
-            break;
-  
-          case 'Search departments':
-            departmentSearch();
-            break;
-  
-          default:
-            console.log(`Invalid action: ${answer.action}`);
-            break;
-        }
-      });
-  };
+const generalSearch = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: ["Search employee", "Search roles", "Search departments", "Add Employee", "Add Role", "Add Department", "Update Employee"],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "Search employee":
+          employeeQuery();
+          break;
+
+        case "Search roles":
+          roleQuery();
+          break;
+
+        case "Search departments":
+          departmentQuery();
+          break;
+
+        case "Add departments":
+          addDepartment();
+          break;
+
+        case "Add roles":
+          addRole();
+          break;
+
+        case "Add employee":
+          addEmployee();
+          break;
+
+        case "Add employee":
+          addEmployee();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
+
+
+
+const employeeQuery = () => {
+    let query = "SELECT * FROM employee";
+        connection.query(query, function(err, res) {
+            console.log(`EMPLOYEES:`)
+        res.forEach(employee => {
+            console.log(`ID: ${employee.id} | Name: ${employee.first_name} ${employee.last_name} | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
+        })
+        generalSearch();
+        });
+    };
+
+    const departmentQuery = () => {
+        let query = "SELECT * FROM department";
+        connection.query(query, function(err, res) {
+            console.log(`DEPARTMENTS:`)
+          res.forEach(department => {
+              console.log(`ID: ${department.id} | Name: ${department.name}`)
+          })
+          generalSearch();
+          });
+      };
+
+      const roleQuery = () => {
+        var query = "SELECT * FROM role";
+        connection.query(query, function(err, res) {
+            console.log(`ROLES:`)
+        res.forEach(role => {
+            console.log(`ID: ${role.id} | Title: ${role.title} | Salary: ${role.salary} | Department ID: ${role.department_id}`);
+        })
+        generalSearch();
+        });
+    };
+
+
+
+
+
+
+
+
+
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "employeefirstname",
+        type: "input",
+        message: "Enter employee first name",
+      },
+      {
+        name: "employeelastname",
+        type: "input",
+        message: "Enter employee last name",
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is their role? ",
+        choices: selectRole(),
+      },
+      {
+        name: "choice",
+        type: "rawlist",
+        message: "Whats their managers name?",
+        choices: selectManager(),
+      },
+    ])
+.then(function (val) {
+    var roleId = selectRole().indexOf(val.role) + 1
+    var managerId = selectManager().indexOf(val.choice) + 1
+    connection.query("INSERT INTO employee SET ?", 
+    {
+        first_name: val.firstName,
+        last_name: val.lastName,
+        manager_id: managerId,
+        role_id: roleId
+        
+    }, function(err){
+        if (err) throw err
+        console.table(val)
+        startPrompt()
+    })
+
+})
+}
+// Add departments, roles, employees
+
+// View departments, roles, employees
+
+// Update employee roles
--- employee_DB.sql
+++ employee_DB.sql
@@ -1,6 +1,6 @@
 -- Drops the employee_db if it exists currently --
 DROP DATABASE IF EXISTS employee_DB;
--- Creates the "employee_db" database --
+-- Creates the "employee_db" database --  
 CREATE DATABASE employee_DB;
 
 -- Makes it so all of the following code will affect employee_db --
@@ -9,9 +9,9 @@ USE employee_DB;
 -- Creates the table "employee", "roles", and "departments" within employee_db --
 CREATE TABLE department (
  id INTEGER NOT NULL auto_increment PRIMARY KEY,
- names VARCHAR(30)
+ name VARCHAR(30)
 );
-CREATE TABLE roles (
+CREATE TABLE role (
 id INTEGER NOT NULL auto_increment PRIMARY KEY,
  title VARCHAR(100) NOT NULL,
  salary DECIMAL (10,2),
@@ -22,10 +22,56 @@ CREATE TABLE employee (
  id INTEGER NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30),
-  roles_id INTEGER,
+  role_id INTEGER,
   manager_id INTEGER NULL,
-  FOREIGN KEY (roles_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES roles(id)
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES role(id)
 );
 
 
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
+
+-- EMPLOYEE ROLE SEEDS -------
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+-- EMPLOYEE SEEDS -------
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jessica", "Haze", null, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tiffany", "Patric", null, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Mia","Lam",null,3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Bently", "Lao", 1, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Chris", "Melby", 4, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jason", "Baker", 1, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tom", "Nice", 2, 7);
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
